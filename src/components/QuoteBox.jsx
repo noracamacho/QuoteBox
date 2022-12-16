c1dbcde9d9f77fc85fb8119774547bfe271ab9eb
noracamacho <49304924+noracamacho@users.noreply.github.com>
2022-12-16 10:41:49 -0600

    Adding favicon

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -4,30 +4,30 @@ import quotes from '../data/quotes'
 
 const QuoteBox = () => {
 
-    const [ index, setIndex ] = useState(Math.floor(Math.random() * quotes.length));
+    const [ index, setIndex ] = useState( Math.floor( Math.random() * quotes.length ) );
     
     const colorGenerator = () =>  {
-        return("#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase());
+        return( "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase() );
     }
 
     const randomIndex = () => {
-        setIndex(Math.floor(Math.random() * quotes.length));
+        setIndex( Math.floor( Math.random() * quotes.length ) );
         colorGenerator();
     }
 
-    const {quote, author}= quotes[index];
+    const { quote, author }= quotes[ index ];
     const selectedColor = colorGenerator();
 
-    document.body.style = `background: ${selectedColor}`;
+    document.body.style = `background: ${ selectedColor }`;
     
   return (
-    <div className='card' style={{color: selectedColor}}>
+    <div className='card' style={{ color: selectedColor }}>
         <i className="fa-solid fa-quote-left quoteIcon"></i>
         <div className='textContainer'>
-            <p className='quote'>{quote}</p>
+            <p className='quote'>{ quote }</p>
         </div>
-            <p className='author'>{author}</p>
-        <button onClick={randomIndex} style={{backgroundColor: selectedColor}}><i className="fa-solid fa-chevron-right"></i></button>
+            <p className='author'>{ author }</p>
+        <button onClick={ randomIndex } style={{ backgroundColor: selectedColor }}><i className="fa-solid fa-chevron-right"></i></button>
     </div>
   )
 }
