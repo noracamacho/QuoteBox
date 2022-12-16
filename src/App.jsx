1ad6da1dafe007f90e0dc578c02e6a18b11da0a1
noracamacho <49304924+noracamacho@users.noreply.github.com>
2022-12-16 01:20:40 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import QuoteBox from './components/QuoteBox'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <QuoteBox />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import quotes from '../data/quotes'
+
+
+const QuoteBox = () => {
+
+    const [ index, setIndex ] = useState(Math.floor(Math.random() * quotes.length));
+    
+    const colorGenerator = () =>  {
+        return("#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase());
+    }
+
+    const randomIndex = () => {
+        setIndex(Math.floor(Math.random() * quotes.length));
+        colorGenerator();
+    }
+
+    const {quote, author}= quotes[index];
+    const selectedColor = colorGenerator();
+
+    document.body.style = `background: ${selectedColor}`;
+    
+  return (
+    <div className='card' style={{color: selectedColor}}>
+        <i className="fa-solid fa-quote-left quoteIcon"></i>
+        <div className='textContainer'>
+            <p className='quote'>{quote}</p>
+        </div>
+            <p className='author'>{author}</p>
+        <button onClick={randomIndex} style={{backgroundColor: selectedColor}}><i className="fa-solid fa-chevron-right"></i></button>
+    </div>
+  )
+}
+
+export default QuoteBox
